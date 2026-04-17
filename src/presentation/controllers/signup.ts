4e1c82571bf98cbda45e5f78956384fdcd02291b
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-17 16:46:35 -0300

    feat: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,8 @@ import { HttpStatusCode } from "../http/http-status-code";
 export class SignUpController {
   handle(httpRequest: unknown): unknown {
     return {
-      statusCode: HttpStatusCode.BAD_REQUEST
+      statusCode: HttpStatusCode.BAD_REQUEST,
+      body: new Error("Missing param: name")
     }
   }
 }
