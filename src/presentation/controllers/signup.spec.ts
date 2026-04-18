1e3bc76bd0a32320b1342a7fdc0eec8123822b83
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-18 15:12:18 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,4 @@
-import { InvalidParamError } from "../errors/invalid-param-error";
-import { MissingParamError } from "../errors/missing-param-error";
-import { ServerError } from "../errors/server-error";
+import { InvalidParamError, MissingParamError, ServerError } from "../errors";
 import { HttpStatusCode } from "../http/http-status-code";
 import type { EmailValidator } from "../protocols/email-validator";
 import type { HttpResponse } from "../protocols/http";
