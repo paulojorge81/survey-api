cc14301cbcd222ab4e8a948f1814153252d90b52
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-19 20:18:56 -0300

    chore: add jest configs and scripts

---
+++ jest-integration.config.js
@@ -0,0 +1,10 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = require('./jest.config')
+config.testMatch = ['**/*.test.ts']
+
+module.exports = config;
