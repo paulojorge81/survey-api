27ab53befcd50d5cab2b1193ff43bf5776d688a9
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-21 20:38:14 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,33 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+
+const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL not defined');
+    }
+    await MongoHelper.connect(process.env.MONGO_URL)
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  });
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+    const accountData = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+    const account = await sut.add(accountData);
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(accountData.name)
+    expect(account.email).toBe(accountData.email)
+    expect(account.password).toBe(accountData.password)
+  });
+});
