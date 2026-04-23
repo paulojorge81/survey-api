58a1ed2f1734a2e7f744e81a6a02b6ae57ffbbf8
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-23 05:21:20 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../config/app';
 
-describe('Body Parser', () => {
+describe('Body Parser Middlewares', () => {
   test('Should parse body as json', async () => {
     const route = '/test_body_parser'
     app.post(route, (req, res) => {
