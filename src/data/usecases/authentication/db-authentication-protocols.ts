56d65fc6f0bea877d18a744ff27c7759f9f0ec38
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-27 16:21:31 -0300

    refactor: move DbAuthentication protocols to a single file

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,6 @@
+export type * from '../../../domain/usecases/authentication';
+export type * from '../../protocols/criptography/hash-comparer';
+export type * from '../../protocols/criptography/token-generator';
+export type * from '../../protocols/db/load-account-by-email-repository';
+export type * from '../../protocols/db/update-access-token-repository';
+export type * from '../add-account/db-add-account-protocols';
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,11 +1,11 @@
 import type {
   Authentication,
   AuthenticationModel,
-} from '../../../domain/usecases/authentication';
-import type { HashComparer } from '../../protocols/criptography/hash-comparer';
-import type { TokenGenerator } from '../../protocols/criptography/token-generator';
-import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
-import type { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
+  HashComparer,
+  LoadAccountByEmailRepository,
+  TokenGenerator,
+  UpdateAccessTokenRepository,
+} from './db-authentication-protocols';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
