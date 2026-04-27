dca0ac8ec74ae82db27aa799868a256d8c0b44ba
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-27 13:36:24 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with corrent email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import type {
+  Authentication,
+  AuthenticationModel,
+} from '../../../domain/usecases/authentication';
+import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository;
+  }
+  async auth(authentication: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email);
+    return '';
+  }
+}
