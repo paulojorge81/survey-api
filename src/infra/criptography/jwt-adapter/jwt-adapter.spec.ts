8db1e1b5886512cbd13b60dc6dfdc19aef68198d
Paulo Jorge <paulojorge@hotmail.com.br>
2026-04-28 10:53:38 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -23,4 +23,13 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_id');
     expect(accessToken).toBe('any_token');
   });
+
+  test('Should throws if sign throws', async () => {
+    const sut = makeSut();
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt('any_id');
+    await expect(promise).rejects.toThrow();
+  });
 });
