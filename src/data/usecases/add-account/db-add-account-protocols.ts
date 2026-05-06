0b85dfd8ed69cd0a903d872bbb838320d7638ea6
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-06 13:33:29 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,5 @@
-export type * from '../../../domain/usecases/add-account';
 export type * from '../../../domain/models/account';
+export type * from '../../../domain/usecases/add-account';
 export type * from '../../protocols/criptography/hasher';
 export type * from '../../protocols/db/account/add-account-repository';
+export type * from '../../protocols/db/account/load-account-by-email-repository';
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -4,19 +4,22 @@ import type {
   AddAccountModel,
   AddAccountRepository,
   Hasher,
+  LoadAccountByEmailRepository,
 } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {}
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
     const hashedPassword = await this.hasher.hash(accountData.password);
     const account = await this.addAccountRepository.add({
       ...accountData,
       password: hashedPassword,
     });
-    return await Promise.resolve(account);
+    return account;
   }
 }
