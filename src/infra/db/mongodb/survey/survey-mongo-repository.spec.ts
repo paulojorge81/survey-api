1431f99056724f3ccefe3312e67e8756c728a550
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-09 10:46:24 -0300

    test: ensure AddSurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,45 @@
+import type { Collection } from 'mongodb';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository';
+
+const makeSut = (): SurveyMongoRepository => new SurveyMongoRepository();
+
+// eslint-disable-next-line @typescript-eslint/init-declarations
+let surveyCollection!: Collection;
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL not defined');
+    }
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany();
+  });
+
+  test('Should add a survey on add success', async () => {
+    const sut = makeSut();
+    const surveyData = {
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer',
+        },
+        {
+          answer: 'other_answer',
+        },
+      ],
+    };
+    await sut.add(surveyData);
+    const survey = await surveyCollection.findOne({ question: 'any_question' });
+    expect(survey).toBeTruthy();
+  });
+});
