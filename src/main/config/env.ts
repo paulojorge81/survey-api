fbb2803bd09b8beb0a88f3b51a978dc875484068
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-12 15:22:26 -0300

    refactor: allows api to run locally or with docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 export const env = {
-  MONGO_URL: process.env.MONGO_URL ?? 'mongodb://admin:secretpassword@mongodb:27017/suvery-api?authSource=admin',
+  MONGO_URL: process.env.MONGO_URL ?? 'mongodb://admin:secretpassword@localhost:27017/suvery-api?authSource=admin',
   PORT: process.env.PORT ?? 5050,
   JWT_SECRET: process.env.JWT_SECRET ?? 'IeHoh8Kieweibu8a',
 };
