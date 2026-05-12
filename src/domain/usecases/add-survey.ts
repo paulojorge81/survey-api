cc4624718a703fb18c1ad831f60d0cd2a6d34975
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-12 15:29:16 -0300

    refactor: change models to use Omit

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,6 @@
-import type { SurveyAnswerModel } from '../models/surveys';
+import type { SurveyModel } from '../models/surveys';
 
-export type AddSurveyModel = {
-  question: string;
-  answers: SurveyAnswerModel[];
-  date: Date;
-};
+export type AddSurveyModel = Omit<SurveyModel, 'id'>;
 
 export interface AddSurvey {
   add: (data: AddSurveyModel) => Promise<void>;
