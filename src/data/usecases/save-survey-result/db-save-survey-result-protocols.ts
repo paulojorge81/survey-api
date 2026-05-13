256a5cb962291a654e2e913c303f5e49be767a92
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-13 15:22:20 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export type * from '@/data/protocols/db/survey/save-survey-result-repository';
+export type * from '@/domain/models/survey-result';
+export type * from '@/domain/usecases/save-survey-result';
