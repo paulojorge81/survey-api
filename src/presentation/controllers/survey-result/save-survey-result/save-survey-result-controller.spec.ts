b3edbb2827817e95601d5faa02c6e58d59fc7af4
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-14 17:11:57 -0300

    test: ensure SaveSurveyResultController returns 403 if an invalid answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -16,6 +16,9 @@ const makeFakeRequest = (): HttpRequest => ({
   params: {
     surveyId: 'any_survey_id',
   },
+  body: {
+    answer: 'any_answer',
+  },
 });
 
 const makeFakeSurvey = (): SurveyModel => ({
@@ -59,6 +62,19 @@ describe('SaveSurveyResult Controller', () => {
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null));
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  test('Should return 403 if an invalid answer is provided', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({
+      params: {
+        surveyId: 'any_survey_id',
+      },
+      body: {
+        answer: 'wrong_answer',
+      },
+    });
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('answer')));
   });
 
   test('Should return 500 if LoadSurveyById throws', async () => {
