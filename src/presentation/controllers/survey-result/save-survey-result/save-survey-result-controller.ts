7d20c708e38affe373701e7724a040b72dd97570
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-14 17:00:58 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,16 +5,20 @@ import type {
   HttpRequest,
   HttpResponse,
 } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols';
-import { forbidden } from '@/presentation/helpers/http/http-helper';
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper';
 import { InvalidParamError } from '@/presentation/errors';
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId);
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'));
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
+      return await Promise.resolve({ body: {}, statusCode: HttpStatusCode.SUCCESS });
+    } catch (error) {
+      return serverError(error instanceof Error ? error : new Error('Internal server error'));
     }
-    return await Promise.resolve({ body: {}, statusCode: HttpStatusCode.SUCCESS });
   }
 }
