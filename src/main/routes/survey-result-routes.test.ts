00dd9d0b7eb27435f215724b64559f2b33e626d7
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-14 21:57:00 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,8 +1,54 @@
+/* eslint-disable @typescript-eslint/init-declarations */
 import request from 'supertest';
 import { HttpStatusCode } from '@/presentation/http/http-status-code';
 import { app } from '@/main/config/app';
+import { ObjectId, type Collection } from 'mongodb';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { sign } from 'jsonwebtoken';
+import { env } from '@/main/config/env';
+
+let surveyCollection!: Collection;
+let accountCollection!: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Paulo',
+    email: 'paulo@mail.com',
+    password: '123',
+    role: 'admin',
+  });
+  const id = res.insertedId.toHexString();
+  const accessToken = sign({ id }, env.JWT_SECRET);
+  await accountCollection.updateOne(
+    {
+      _id: new ObjectId(id),
+    },
+    {
+      $set: { accessToken },
+    },
+  );
+  return accessToken;
+};
 
 describe('Survey Results Routes', () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL not defined');
+    }
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany();
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany();
+  });
+
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on save surveys without accessToken', async () => {
       const route = '/api/surveys/any_id/results';
@@ -12,6 +58,33 @@ describe('Survey Results Routes', () => {
           answer: 'any_answer',
         })
         .expect(HttpStatusCode.FORBIDDEN);
+    });
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            image: 'http://image-name.com',
+            answer: 'answer 1',
+          },
+          {
+            answer: 'answer 2',
+          },
+        ],
+        date: new Date(),
+      });
+
+      const route = `/api/surveys/${res.insertedId.toHexString()}/results`;
+
+      await request(app)
+        .put(route)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'answer 1',
+        })
+        .expect(HttpStatusCode.SUCCESS);
     });
   });
 });
