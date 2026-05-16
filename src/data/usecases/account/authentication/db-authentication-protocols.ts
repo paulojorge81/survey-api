07ed4e619b5036a3a62cf4ee96dc69d1117d5d7d
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-15 23:35:53 -0300

    fix: fix imports add account

--- src/data/usecases/account/authentication/db-authentication-protocols.ts
+++ src/data/usecases/account/authentication/db-authentication-protocols.ts
@@ -2,5 +2,5 @@ export type * from '@/data/protocols/criptography/encrypter';
 export type * from '@/data/protocols/criptography/hash-comparer';
 export type * from '@/data/protocols/db/account/load-account-by-email-repository';
 export type * from '@/data/protocols/db/account/update-access-token-repository';
-export type * from '@/data/usecases/add-account/db-add-account-protocols';
+export type * from '@/data/usecases/account/add-account/db-add-account-protocols';
 export type * from '@/domain/usecases/account/authentication';
