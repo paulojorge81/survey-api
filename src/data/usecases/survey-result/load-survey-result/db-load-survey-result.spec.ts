26bf696837817d935ee9069371e76171c11ec849
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-18 22:45:07 -0300

    test: ensure DbLoadSurveyResult throws LoadSurveyResultRepository if throws

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -21,4 +21,11 @@ describe('DbLoadSurveyResult UseCase', () => {
     await sut.load('any_survey_id');
     expect(loadBySurveyId).toHaveBeenCalledWith('any_survey_id');
   });
+
+  test('Should throws if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockRejectedValueOnce(new Error());
+    const promise = sut.load('any_survey_id');
+    await expect(promise).rejects.toThrow();
+  });
 });
