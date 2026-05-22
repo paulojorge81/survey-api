e3b0905a887ca8a261930f057f4830c8f189af48
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-22 15:09:04 -0300

    feat: ensure load survey route returns 403 if no accessToken if provided

---
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result/load-survey-result-controller-factory.ts
@@ -0,0 +1,11 @@
+import type { Controller } from '@/presentation/protocols';
+
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory';
+import { makeDbLoadSurveyResult } from '@/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory';
+import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory';
+import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller';
+
+export const makeLoadSurveyResultController = (): Controller => {
+  const controller = new LoadSurveyResultController(makeDbLoadSurveyById(), makeDbLoadSurveyResult());
+  return makeLogControllerDecorator(controller);
+};
--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,9 +1,11 @@
 import type { Router } from 'express';
 
 import { adaptRoute } from '@/main/adapters/express/express-routes-adapter';
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result/load-survey-result-controller-factory';
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory';
 import { auth } from '@/main/middlewares/auth';
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()));
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()));
 };
