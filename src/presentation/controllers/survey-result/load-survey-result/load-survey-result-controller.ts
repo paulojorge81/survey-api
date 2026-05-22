f3b820ca2643123e2c6d928e253d5c844df9060a
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-22 14:27:30 -0300

    feat: ensure LoadSurveyResultController returns 403 if an invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -5,19 +5,31 @@ import type {
   LoadSurveyById,
 } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols';
 
+import { InvalidParamError } from '@/presentation/errors';
+import { forbidden } from '@/presentation/helpers/http/http-helper';
 import { HttpStatusCode } from '@/presentation/http/http-status-code';
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle(httpeRequest: HttpRequest): Promise<HttpResponse> {
-    const {
-      params: { surveyId },
-    } = httpeRequest;
-    await this.loadSurveyById.loadById(surveyId);
-    return await Promise.resolve({
-      body: {},
-      statusCode: HttpStatusCode.SUCCESS,
-    });
+    try {
+      const {
+        params: { surveyId },
+      } = httpeRequest;
+      const survey = await this.loadSurveyById.loadById(surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
+      return await Promise.resolve({
+        body: {},
+        statusCode: HttpStatusCode.SUCCESS,
+      });
+    } catch (error) {
+      return {
+        body: {},
+        statusCode: HttpStatusCode.SERVER_ERROR,
+      };
+    }
   }
 }
