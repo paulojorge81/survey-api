4ec2ebbc78674e0bdfd49c30a580be24eb766def
Paulo Jorge <paulojorge@hotmail.com.br>
2026-05-31 19:30:17 -0300

    feat: add status code error on exceptions

--- src/main/adapters/graphql/apollo-server-resolver-adapter.ts
+++ src/main/adapters/graphql/apollo-server-resolver-adapter.ts
@@ -1,6 +1,27 @@
+import { GraphQLError } from 'graphql';
+
 import type { Controller } from '@/presentation/protocols';
+
+import { HttpStatusCode } from '@/presentation/http';
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args);
-  return httpResponse.body;
+  switch (httpResponse.statusCode) {
+    case HttpStatusCode.SUCCESS:
+    case HttpStatusCode.NO_CONTENT:
+      return httpResponse.body;
+    case HttpStatusCode.BAD_REQUEST:
+      throw makeGraphQLError(httpResponse.body.message, 'BAD_USER_INPUT');
+    case HttpStatusCode.UNAUTHORIZED:
+      throw makeGraphQLError(httpResponse.body.message, 'UNAUTHENTICATED');
+    case HttpStatusCode.FORBIDDEN:
+      throw makeGraphQLError(httpResponse.body.message, 'FORBIDDEN');
+    default:
+      throw makeGraphQLError(httpResponse.body.message, 'INTERNAL_SERVER_ERROR');
+  }
 };
+
+const makeGraphQLError = (message: string, code: string): Error =>
+  new GraphQLError(message, {
+    extensions: { code },
+  });
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,13 +1,50 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/prefer-destructuring */
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import type { Express } from 'express';
 
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer, type ApolloServerPlugin } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
 
 import resolvers from '@/main/graphql/resolvers';
 import typeDefs from '@/main/graphql/type-defs';
+import { HttpStatusCode } from '@/presentation/http';
 
 export const setupApolloServer = async (app: Express): Promise<void> => {
-  const server = new ApolloServer({ resolvers, typeDefs });
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    plugins: [statusCodePlugin],
+  });
   await server.start();
   app.use('/graphql', expressMiddleware(server));
+};
+
+const statusCodePlugin: ApolloServerPlugin = {
+  async requestDidStart() {
+    return await Promise.resolve({
+      async willSendResponse({ response, errors }: any) {
+        const code = errors?.[0]?.extensions?.code;
+        if (!response.http) {
+          return;
+        }
+        response.data = undefined;
+        switch (code) {
+          case 'BAD_USER_INPUT':
+            response.http.status = HttpStatusCode.BAD_REQUEST;
+            break;
+          case 'UNAUTHENTICATED':
+            response.http.status = HttpStatusCode.UNAUTHORIZED;
+            break;
+          case 'FORBIDDEN':
+            response.http.status = HttpStatusCode.FORBIDDEN;
+            break;
+          default:
+            response.http.status = HttpStatusCode.SERVER_ERROR;
+            break;
+        }
+      },
+    });
+  },
 };
