eeffa223b7834f94f9f343da1603cc8634a14ba5
Paulo Jorge <paulojorge@hotmail.com.br>
2026-06-04 14:38:46 -0300

    test: ensure Surveys Query returns AccessDeniedError if no token is provided

--- tests/main/graphql/survey.test.ts
+++ tests/main/graphql/survey.test.ts
@@ -110,5 +110,47 @@ describe('Survey GraphQL', () => {
         },
       ]);
     });
+
+    test('Should return AccessDeniedError if no token is provides', async () => {
+      const now = new Date();
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            image: 'http://image-name.com',
+            answer: 'answer 1',
+          },
+          {
+            answer: 'answer 2',
+          },
+        ],
+        date: now,
+      });
+      const app = await makeApp();
+      const {
+        body: { data, errors },
+      } = await request(app)
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              surveys {
+                  id
+                  question
+                  answers {
+                      image
+                      answer
+                  }
+                  didAnswer
+                  date
+              }
+            }
+          `,
+        })
+        .expect(HttpStatusCode.FORBIDDEN);
+
+      expect(data).toBeFalsy();
+      expect(errors[0].message).toBe('Access denied');
+    });
   });
 });
